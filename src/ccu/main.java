16c17d648f055e581b7840c7b6051610eb372bfb
CUCHOI <CUCHOI@CUCHOI-PC>
2021-01-26 16:35:20 +0900

    두번째 member add

---
+++ src/ccu/main.java
@@ -0,0 +1,10 @@
+package ccu;
+
+public class main {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
---
+++ src/ccu/member.java
@@ -0,0 +1,10 @@
+package ccu;
+
+public class member {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
